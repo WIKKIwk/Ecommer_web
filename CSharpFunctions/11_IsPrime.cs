685a8cf4a6978cff21e733707e040bd7c7891d46
WIKKI <uzkingshark@gmail.com>
2026-01-10 14:49:47 +0500

    Add IsPrime function

---
+++ CSharpFunctions/11_IsPrime.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace CSharpFunctions
+{
+    public class MathUtilities
+    {
+        /// <summary>
+        /// Checks if a number is prime
+        /// </summary>
+        public static bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+            
+            for (int i = 3; i * i <= number; i += 2)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            
+            return true;
+        }
+    }
+}
