afb13b2abc035599340386a4e9dc53fa2a48c926
WIKKI <uzkingshark@gmail.com>
2026-01-11 16:51:29 +0500

    Add Category admin

---
+++ shop2/backend/store/admin.py
@@ -0,0 +1,9 @@
+"""Django Admin"""
+from django.contrib import admin
+from .models import Category, Product, Cart, CartItem, Order, OrderItem
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'created_at']
+    prepopulated_fields = {'slug': ('name',)}
