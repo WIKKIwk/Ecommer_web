1e60734ef3b287ee917e81c733b36f14776c31cc
WIKKI <uzkingshark@gmail.com>
2026-01-11 16:49:55 +0500

    Add OrderItem model

--- shop2/backend/store/models.py
+++ shop2/backend/store/models.py
@@ -72,5 +72,17 @@ class Order(models.Model):
         return f'Order {self.id}'
 
 
+class OrderItem(models.Model):
+    """Item in order"""
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.product.name} x{self.quantity}'
+
+
+
 
 
