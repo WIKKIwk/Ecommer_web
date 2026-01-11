9da90ea41131a699ed7236d7e93a2d967f5e655b
WIKKI <uzkingshark@gmail.com>
2026-01-11 16:49:38 +0500

    Add Product model

--- shop2/backend/store/models.py
+++ shop2/backend/store/models.py
@@ -14,3 +14,18 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    """Product"""
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
