870738c7adac0d07213235d8f505e24b0023eeba
WIKKI <uzkingshark@gmail.com>
2026-01-25 14:04:03 +0500

    Add formatting utility functions

---
+++ JSFunctions/11_formatting.js
@@ -0,0 +1,37 @@
+/**
+ * Formatting utilities
+ */
+
+/**
+ * Format number as currency
+ * @param {number} amount - Amount to format
+ * @param {string} currency - Currency code
+ * @returns {string} Formatted currency
+ */
+function formatCurrency(amount, currency = 'USD') {
+    return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: currency
+    }).format(amount);
+}
+
+/**
+ * Format number with thousand separators
+ * @param {number} num - Number to format
+ * @returns {string} Formatted number
+ */
+function formatNumber(num) {
+    return num.toLocaleString();
+}
+
+/**
+ * Truncate text with ellipsis
+ * @param {string} text - Text to truncate
+ * @param {number} length - Max length
+ * @returns {string} Truncated text
+ */
+function truncate(text, length) {
+    return text.length > length ? text.substring(0, length) + '...' : text;
+}
+
+module.exports = { formatCurrency, formatNumber, truncate };
