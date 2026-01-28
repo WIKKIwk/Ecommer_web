c462000e8396857bd6af6014f86633dc33060227
WIKKI <uzkingshark@gmail.com>
2026-01-28 15:14:34 +0500

    Add escape HTML utility

---
+++ JSFunctions/147_escape_html.js
@@ -0,0 +1,19 @@
+/**
+ * Escape HTML Utility
+ */
+
+/**
+ * Escape HTML special characters
+ * @param {string} unsafe 
+ * @returns {string} Escaped string
+ */
+function escapeHtml(unsafe) {
+  return unsafe
+         .replace(/&/g, "&amp;")
+         .replace(/</g, "&lt;")
+         .replace(/>/g, "&gt;")
+         .replace(/"/g, "&quot;")
+         .replace(/'/g, "&#039;");
+}
+
+module.exports = { escapeHtml };
