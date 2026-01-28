df4eefe3d4b212b57d2cc5c31cbd545e69f37bdd
WIKKI <uzkingshark@gmail.com>
2026-01-28 14:10:30 +0500

    Add Priority Queue data structure

---
+++ JSFunctions/81_priority_queue.js
@@ -0,0 +1,40 @@
+/**
+ * Priority Queue
+ */
+
+class PriorityQueue {
+  constructor() {
+    this.items = [];
+  }
+
+  enqueue(element, priority) {
+    const queueElement = { element, priority };
+    let added = false;
+    
+    for (let i = 0; i < this.items.length; i++) {
+      if (queueElement.priority < this.items[i].priority) {
+        this.items.splice(i, 0, queueElement);
+        added = true;
+        break;
+      }
+    }
+    
+    if (!added) {
+      this.items.push(queueElement);
+    }
+  }
+
+  dequeue() {
+    return this.items.shift();
+  }
+
+  front() {
+    return this.items[0];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+}
+
+module.exports = PriorityQueue;
