f25d4521a4a198d07b8cf5e4fb059c0fb795c4d6
WIKKI <uzkingshark@gmail.com>
2026-01-28 14:08:41 +0500

    Add HTTP client wrapper

---
+++ JSFunctions/70_http.js
@@ -0,0 +1,39 @@
+/**
+ * HTTP Client Wrapper
+ */
+
+class HttpClient {
+  constructor(baseUrl = '') {
+    this.baseUrl = baseUrl;
+  }
+
+  async request(endpoint, options = {}) {
+    const url = `${this.baseUrl}${endpoint}`;
+    const response = await fetch(url, {
+      ...options,
+      headers: {
+        'Content-Type': 'application/json',
+        ...options.headers
+      }
+    });
+    
+    if (!response.ok) {
+      throw new Error(`HTTP Error: ${response.status}`);
+    }
+    
+    return response.json();
+  }
+
+  get(endpoint) {
+    return this.request(endpoint, { method: 'GET' });
+  }
+
+  post(endpoint, data) {
+    return this.request(endpoint, { 
+      method: 'POST', 
+      body: JSON.stringify(data) 
+    });
+  }
+}
+
+module.exports = HttpClient;
