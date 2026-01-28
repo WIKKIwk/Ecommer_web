b097dd389dc0e5cf405e290debb0da1d90e109c5
WIKKI <uzkingshark@gmail.com>
2026-01-28 13:39:56 +0500

    Add groupby utility

---
+++ JSFunctions/52_groupby.js
@@ -0,0 +1,19 @@
+/**
+ * GroupBy Utility
+ */
+
+/**
+ * Group array by key
+ * @param {Array} collection 
+ * @param {string|Function} iteratee 
+ * @returns {Object} Grouped object
+ */
+function groupBy(collection, iteratee) {
+  return collection.reduce((result, item) => {
+    const key = typeof iteratee === 'function' ? iteratee(item) : item[iteratee];
+    (result[key] = result[key] || []).push(item);
+    return result;
+  }, {});
+}
+
+module.exports = { groupBy };
