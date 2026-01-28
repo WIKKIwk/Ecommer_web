67cffc0b66e1962cab5cffe428810a2c5dab6368
WIKKI <uzkingshark@gmail.com>
2026-01-28 14:08:11 +0500

    Add Observable pattern

---
+++ JSFunctions/61_observable.js
@@ -0,0 +1,24 @@
+/**
+ * Observable Pattern
+ */
+
+class Observable {
+  constructor() {
+    this.observers = [];
+  }
+
+  subscribe(fn) {
+    this.observers.push(fn);
+    return () => this.unsubscribe(fn);
+  }
+
+  unsubscribe(fn) {
+    this.observers = this.observers.filter(sub => sub !== fn);
+  }
+
+  notify(data) {
+    this.observers.forEach(observer => observer(data));
+  }
+}
+
+module.exports = Observable;
