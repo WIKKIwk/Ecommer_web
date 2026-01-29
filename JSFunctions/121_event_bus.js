b2e8c6982c2fe46a7155f1998c9bc5f3c3025229
WIKKI <uzkingshark@gmail.com>
2026-01-29 11:57:40 +0500

    Add Event Bus

---
+++ JSFunctions/121_event_bus.js
@@ -0,0 +1,36 @@
+/**
+ * Simple Event Bus (Pub/Sub)
+ */
+
+class EventBus {
+    constructor() {
+        this.listeners = {};
+    }
+
+    on(event, callback) {
+        if (!this.listeners[event]) {
+            this.listeners[event] = [];
+        }
+        this.listeners[event].push(callback);
+        return () => this.off(event, callback);
+    }
+
+    off(event, callback) {
+        if (!this.listeners[event]) return;
+        this.listeners[event] = this.listeners[event].filter(cb => cb !== callback);
+    }
+
+    emit(event, data) {
+        if (!this.listeners[event]) return;
+        this.listeners[event].forEach(callback => callback(data));
+    }
+
+    once(event, callback) {
+        const off = this.on(event, (data) => {
+            off();
+            callback(data);
+        });
+    }
+}
+
+module.exports = EventBus;
