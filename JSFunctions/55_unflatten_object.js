763d3d78ed20b4019bfd561dbccfb2d940261413
WIKKI <uzkingshark@gmail.com>
2026-01-29 10:50:58 +0500

    Add Unflatten Object

---
+++ JSFunctions/55_unflatten_object.js
@@ -0,0 +1,21 @@
+/**
+ * Unflatten Object Utility
+ */
+
+/**
+ * Unflatten object from dot notation
+ * @param {Object} obj 
+ * @returns {Object} Nested object
+ */
+function unflattenObject(obj) {
+    const result = {};
+    for (const i in obj) {
+        const keys = i.split('.');
+        keys.reduce((acc, part, index) => {
+            return acc[part] || (acc[part] = isNaN(Number(keys[index + 1])) ? keys.length - 1 === index ? obj[i] : {} : []);
+        }, result);
+    }
+    return result;
+}
+
+module.exports = { unflattenObject };
