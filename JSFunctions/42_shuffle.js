7bf77e80093895c9e6663d7297b636cefe059aa6
WIKKI <uzkingshark@gmail.com>
2026-01-29 10:50:16 +0500

    Add Array Shuffle utility

---
+++ JSFunctions/42_shuffle.js
@@ -0,0 +1,23 @@
+/**
+ * Array Shuffle
+ */
+
+/**
+ * Fisher-Yates Shuffle
+ * @param {Array} array 
+ * @returns {Array} Shuffled array
+ */
+function shuffle(array) {
+    let currentIndex = array.length, randomIndex;
+
+    while (currentIndex != 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+}
+
+module.exports = { shuffle };
