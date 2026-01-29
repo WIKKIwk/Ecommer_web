c4f5845a121efb5db7029efdb721c5c15b83cbf3
WIKKI <uzkingshark@gmail.com>
2026-01-29 10:59:56 +0500

    Add Template Engine

---
+++ JSFunctions/64_template.js
@@ -0,0 +1,17 @@
+/**
+ * Simple Template Engine
+ */
+
+/**
+ * Interpolate string with data
+ * @param {string} template 
+ * @param {Object} data 
+ * @returns {string}
+ */
+function render(template, data) {
+    return template.replace(/\{\{(\w+)\}\}/g, (match, key) => {
+        return data[key] !== undefined ? data[key] : match;
+    });
+}
+
+module.exports = { render };
